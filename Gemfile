78e3d79bcf981f0f6c52d15ff033eebb5c69ddcf
kami <kami30k@gmail.com>
2015-07-04 01:02:04 +0900

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in default_value_for-matchers.gemspec
+gemspec
---
+++ lib/default_value_for/matchers.rb
@@ -0,0 +1,7 @@
+require "default_value_for/matchers/version"
+
+module DefaultValueFor
+  module Matchers
+    # Your code goes here...
+  end
+end
